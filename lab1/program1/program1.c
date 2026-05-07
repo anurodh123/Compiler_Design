4d7ed444ad50665de9b735773f766070423b8f55
anurodh123 <168972711+anurodh123@users.noreply.github.com>
2026-05-07 17:13:13 +0545

    Add files via upload

---
+++ lab1/program1/program1.c
@@ -0,0 +1,18 @@
+
+/* This is a simple C program that takes two integers as input from the user,
+calculates their sum, and prints the result.
+It uses the standard input/output library for handling user input and output.
+  The program follows the C17 language standard version. */
+
+/* #include <stdio.h>
+int main() {
+int a, b, sum;
+printf("Enter two numbers: ");
+scanf("%d %d", &a, &b);
+sum = a + b;
+printf("Sum = %d\n", sum);
+return 0;
+}
+ */
+
+
